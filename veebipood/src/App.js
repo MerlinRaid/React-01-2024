3846aff1a7221778ee5965877170a9ea9def6652
MerlinRaid <merlin.raid@gmail.com>
2024-01-18 12:31:37 +0200

    Uus reacti rakendus

---
+++ veebipood/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" className="pilt" alt="Logo" />
+      <button className="nupp">Nupp</button>
+      <div>Tekst</div>
+    </div>
+  );
+}
+
+export default App;
