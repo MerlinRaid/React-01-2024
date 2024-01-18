b5e6e60d071e8156146546edb344ff7d14ce23e2
MerlinRaid <merlin.raid@gmail.com>
2024-01-18 22:15:07 +0200

    Kodutoo1

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="nupp">Nupp</button>
+      <div className="tekst">Tekst</div>
+      <h2 className="tekst2">Tekst</h2>
+      <img src="https://t4.ftcdn.net/jpg/05/74/26/31/360_F_574263101_TJBaEd9BGd36eO1TlPhnl8apExNEdq8n.jpg" className="pilt" alt="Logo" />
+      
+
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/VIDEO_ID"
+      ></iframe>
+    </div>
+    
+  );
+}
+
+export default App;
