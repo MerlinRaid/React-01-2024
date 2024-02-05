8984e9bf87d1d41a06161d29f57e943dc91f1696
MerlinRaid <merlin.raid@gmail.com>
2024-02-05 19:52:00 +0200

    jätk telefoni värv

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -4,6 +4,8 @@ import { Link, Navigate, Route, Routes } from 'react-router-dom'
 import Ilmumine from './Pages/Ilmumine';
 import Kujundus from './Pages/Kujundus';
 import Muutmine from './Pages/Muutmine';
+import Telefon from './Pages/Telefon';
+import Hind from './Pages/Hind';
 
 function App() {
   return (
@@ -11,11 +13,15 @@ function App() {
       <Link to="/ilmumine"><button>Ilmumine</button></Link>
       <Link to="/kujundus"><button>Kujundus</button></Link>
       <Link to="/muutmine"><button>Muutmine</button></Link>
+      <Link to="/telefon"><button>Telefon</button></Link>
+      <Link to="/hind"><button>Hind</button></Link>
      <Routes>
      <Route path="/" element={ <Navigate to="/ilmumine" /> } />
       <Route path="/ilmumine" element={ <Ilmumine /> } />
       <Route path="/kujundus" element={ <Kujundus /> } />
       <Route path="/muutmine" element={ <Muutmine /> } />
+      <Route path="/telefon" element={ <Telefon /> } />
+      <Route path="/hind" element={ <Hind /> } />
      </Routes>
   </div>
   );
---
+++ usestate/src/Pages/Hind.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hind() {
+  return (
+    <div>Hind</div>
+  )
+}
+
+export default Hind
