5c52f51fcb5c1c35e174203e53c6c8755345aff5
MerlinRaid <merlin.raid@gmail.com>
2024-02-09 12:17:06 +0200

    useState_kontaktide_kuvamine

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -1,19 +1,26 @@
+import { useState } from "react";
+
 function Kontakt() {
+    const [ n2itaTelKristiine, muudaN2itaTelKristiine] = useState(false)
+    const [ n2itaTelYlemiste, muudaN2itaTelYlemiste] = useState(false)
+    const [ n2itaTelTasku, muudaN2itaTelTasku] = useState(false)
+
+
     return ( <div>
         <div> See on kontaktide leht, nähtav aadressil localhost:3000/kontakt </div>
         <div>Võta meiega ühendust: </div>
+        <br /> 
+        <div className={n2itaTelKristiine === true ? "valitud" : undefined} onClick={() => muudaN2itaTelKristiine(!n2itaTelKristiine)}>Kristiine keskus</div>
+        {n2itaTelKristiine && <div className="valitud">+351231231</div>}
+        <div className={n2itaTelKristiine === true ? "valitud" : undefined}>Endla 45</div>
         <br />
-        <div>Kristiine keskus</div>
-        <div>+351231231</div>
-        <div>Endla 45</div>
+        <div className={n2itaTelYlemiste === true ? "valitud" : undefined} onClick={() => muudaN2itaTelYlemiste(!n2itaTelYlemiste)}>Ülemiste keskus</div>
+        {n2itaTelYlemiste && <div div className="valitud">+34151231</div>}
+        <div className={n2itaTelYlemiste === true ? "valitud" : undefined}>Suur-Sõjamäe 4</div>
         <br />
-        <div>Ülemiste keskus</div>
-        <div>+34151231</div>
-        <div>Suur-Sõjamäe 4</div>
-        <br />
-        <div>Tasku keskus</div>
-        <div>+39312323</div>
-        <div>Turu 2</div>
+        <div className={n2itaTelTasku === true ? "valitud" : undefined} onClick={() => muudaN2itaTelTasku(!n2itaTelTasku )}>Tasku keskus</div>
+        {n2itaTelTasku && <div div className="valitud">+39312323</div>}
+        <div className={n2itaTelTasku === true ? "valitud" : undefined} >Turu 2</div>
     </div> );
 }
 
