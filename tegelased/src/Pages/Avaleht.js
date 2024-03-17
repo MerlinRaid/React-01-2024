fda6d5bdefbff390a4976a84c0412ed2155f8d87
MerlinRaid <merlin.raid@gmail.com>
2024-03-17 14:19:34 +0200

    useState, map. kuvamine

--- tegelased/src/Pages/Avaleht.js
+++ tegelased/src/Pages/Avaleht.js
@@ -1,10 +1,35 @@
-
+import { useState } from "react"
 
 function Avaleht() {
+    const tegelased = [
+        {eesnimi: "Mickey", perenimi: "Mouse", elukoht: "Disneyland"},
+        {eesnimi: "Minnie", perenimi: "Mouse", elukoht: "Disneyland"},
+        {eesnimi: "Winnie", perenimi: "Pooh", elukoht: "Hundred Acre Wood"},
+        {eesnimi: "Roo", perenimi: "Kangaroo", elukoht: "Hundred Acre Wood"},
+        {eesnimi: "Scooby", perenimi: "Doo", elukoht: "Crystal Cove"},
+    ]
+
+    const [klikitudNimi,uuendaKlikitudNimi] = useState ("");
+
+    const kuvaNimi = (tegelane) => {
+        //console.log(tegelane.eesnimi);
+        uuendaKlikitudNimi(tegelane.eesnimi);
+        }
+
 
   return (
 <div>
-    <div>
+    
+    { klikitudNimi !== "" && <div>Klikisid tegelase {klikitudNimi} peal</div>}
+     {tegelased.map (tegelane => <div>
+        <div>{tegelane.eesnimi}</div>
+        <div>{tegelane.perenimi}</div>
+        <div>{tegelane.elukoht}</div>
+        <button onClick={() => kuvaNimi(tegelane)}  >Kuva nimi</button>
+        <br /><br />
+
+     </div> )}
+    {/* <div>
             <div>Mickey</div>
             <div>Mouse</div>
             <div>Disneyland</div>
@@ -28,7 +53,7 @@ function Avaleht() {
         <div>Scooby</div>
         <div>Doo</div>
         <div>Crystal Cove</div>
-    </div>
+    </div> */}
 </div>
     
 
