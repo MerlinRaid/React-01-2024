452b6baac82cdffb9cf8912e32cdf57e433b3b94
MerlinRaid <merlin.raid@gmail.com>
2024-04-08 22:15:33 +0300

    õige demo

--- beebiraamat/src/components/Demo.js
+++ beebiraamat/src/components/Demo.js
@@ -15,7 +15,7 @@ const Demo = () => {
                     </Link>
                 </div>
                 <div className='col-2'>
-                    <iframe width='570' height='320' src='https://www.youtube.com/embed/oeqP5JtihMA' title='Youtube video player' frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                    <iframe width='560' height='315' src="https://www.youtube.com/embed/HkbnS0jGICc?si=UmfBc-91JMpdM_F2" title='Youtube video player' frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
                 </div>
             </div>
         </div>
